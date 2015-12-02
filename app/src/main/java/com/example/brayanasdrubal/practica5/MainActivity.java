09f9da3fde83256b47ec76586735c4c26361ebfa
BrayanAsdrubal <brayan_ahg@hotmail.com>
2015-12-02 00:12:09 -0500

    Ultimo

--- app/src/main/java/com/example/brayanasdrubal/practica5/MainActivity.java
+++ app/src/main/java/com/example/brayanasdrubal/practica5/MainActivity.java
@@ -36,129 +36,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(findViewById(R.id.lay2)!=null){
-            TbH = (TabHost) findViewById(R.id.tabHost); //llamamos al Tabhost
-            TbH.setup();                                                         //lo activamos
 
-            TabHost.TabSpec tab1 = TbH.newTabSpec("tab1");  //aspectos de cada Tab (pestaña)
-            TabHost.TabSpec tab2 = TbH.newTabSpec("tab2");
-            TabHost.TabSpec tab3 = TbH.newTabSpec("tab3");
-
-
-            if(num ==1){
-                l=3;
-            }else if(num ==2){
-                l=5;
-            }else if(num ==3){
-                l=8;
-            }else if(num ==4){
-                l=11;
-            }
-
-            c1t=R.string.cine1t;
-            c2t=R.string.cine2t;
-            c3t=R.string.cine3t;
-            c1=R.string.cine1;
-            c2=R.string.cine2;
-            c3=R.string.cine3;
-            t1t=R.string.teatro1t;
-            t2t=R.string.teatro2t;
-            te1=R.string.teatro1;
-            te2=R.string.teatro2;
-            r1t=R.string.rest1t;
-            r2t=R.string.rest2t;
-            r3t=R.string.rest3t;
-            r1=R.string.rest1;
-            r2=R.string.rest2;
-            r3=R.string.rest3;
-            ru1t=R.string.rumba1t;
-            ru2t=R.string.rumba2t;
-            ru3t=R.string.rumba3t;
-            ru1=R.string.rumba1;
-            ru2=R.string.rumba2;
-            ru3=R.string.rumba3;
-            tu1t=R.string.turis1t;
-            tu2t=R.string.turis2t;
-            tu3t=R.string.turis3t;
-            tu1=R.string.turis1;
-            tu2=R.string.turis2;
-            tu3=R.string.turis3;
-
-
-            tab1.setIndicator(lugares[l]);    //qué queremos que aparezca en las pestañas
-            tab1.setContent(R.id.ejemplo1); //definimos el id de cada Tab (pestaña)
-            tab2.setIndicator(lugares[l + 1]);
-            tab2.setContent(R.id.ejemplo2);
-            tab3.setIndicator(lugares[l + 2]);
-            tab3.setContent(R.id.ejemplo3);
-            TbH.addTab(tab1); //añadimos los tabs ya programados
-            TbH.addTab(tab2);
-            if (MainActivity.num !=1){
-                TbH.addTab(tab3);
-            }
-
-            ImageView sit1 = (ImageView) findViewById(R.id.drw1);
-            ImageView sit2 = (ImageView) findViewById(R.id.drw2);
-            ImageView sit3 = (ImageView) findViewById(R.id.drw3);
-            TextView t1 = (TextView) findViewById(R.id.tit1);
-            TextView tx1 = (TextView) findViewById(R.id.tex1);
-            TextView t2 = (TextView) findViewById(R.id.tit2);
-            TextView tx2 = (TextView) findViewById(R.id.tex2);
-            TextView t3 = (TextView) findViewById(R.id.tit3);
-            TextView tx3 = (TextView) findViewById(R.id.tex3);
-            if(MainActivity.num ==0){
-                setTitle(getText(R.string.Cine));
-                sit1.setImageResource(R.drawable.cine1);
-                t1.setText(c1t);
-                tx1.setText(c1);
-                sit2.setImageResource(R.drawable.cine2);
-                t2.setText(c2t);
-                tx2.setText(c2);
-                sit3.setImageResource(R.drawable.cine3);
-                t3.setText(c3t);
-                tx3.setText(c3);
-            }else if(MainActivity.num ==1){
-                setTitle(getText(R.string.Teatro));
-                sit1.setImageResource(R.drawable.teatro1);
-                t1.setText(t1t);
-                tx1.setText(te1);
-                sit2.setImageResource(R.drawable.teatro2);
-                t2.setText(t2t);
-                tx2.setText(te2);
-            }else if(MainActivity.num ==2){
-                setTitle(getText(R.string.Restaurante));
-                sit1.setImageResource(R.drawable.rest1);
-                t1.setText(r1t);
-                tx1.setText(r1);
-                sit2.setImageResource(R.drawable.rest2);
-                t2.setText(r2t);
-                tx2.setText(r2);
-                sit3.setImageResource(R.drawable.rest3);
-                t3.setText(r3t);
-                tx3.setText(r3);
-            }else if(MainActivity.num ==3){
-                setTitle(getText(R.string.Rumba));
-                sit1.setImageResource(R.drawable.rumba1);
-                t1.setText(ru1t);
-                tx1.setText(ru1);
-                sit2.setImageResource(R.drawable.rumba2);
-                t2.setText(ru2t);
-                tx2.setText(ru2);
-                sit3.setImageResource(R.drawable.rumba3);
-                t3.setText(ru3t);
-                tx3.setText(ru3);
-            }else if(MainActivity.num ==4){
-                setTitle(getText(R.string.Turismo));
-                sit1.setImageResource(R.drawable.turi1);
-                t1.setText(tu1t);
-                tx1.setText(tu1);
-                sit2.setImageResource(R.drawable.turi2);
-                t2.setText(tu2t);
-                tx2.setText(tu2);
-                sit3.setImageResource(R.drawable.turi3);
-                t3.setText(tu3t);
-                tx3.setText(tu3);
-            }}
         datos= new Lista_Entrada[]{
                 new Lista_Entrada(R.drawable.cine, getString(R.string.Cine), "3 "+getString(R.string.L),"Cinema","3 Places"),
                 new Lista_Entrada(R.drawable.teatro, getString(R.string.Teatro), "2 "+getString(R.string.L), "Theatre","2 Places"),
@@ -185,15 +63,145 @@ public class MainActivity extends AppCompatActivity {
             fragmentTransaction.add(R.id.framelayout_right,animalDetailFragment);
         }*/
 
+            if (findViewById(R.id.lay2) != null){
+                TbH = (TabHost) findViewById(R.id.tabHost); //llamamos al Tabhost
+                TbH.setup();                                                         //lo activamos
+                TabHost.TabSpec tab1 = TbH.newTabSpec("tab1");  //aspectos de cada Tab (pestaña)
+                TabHost.TabSpec tab2 = TbH.newTabSpec("tab2");
+                TabHost.TabSpec tab3 = TbH.newTabSpec("tab3");
+
+                if(num ==1){
+                    l=3;
+                }else if(num ==2){
+                    l=5;
+                }else if(num ==3){
+                    l=8;
+                }else if(num ==4){
+                    l=11;
+                }
+
+                c1t=R.string.cine1t;
+                c2t=R.string.cine2t;
+                c3t=R.string.cine3t;
+                c1=R.string.cine1;
+                c2=R.string.cine2;
+                c3=R.string.cine3;
+                t1t=R.string.teatro1t;
+                t2t=R.string.teatro2t;
+                te1=R.string.teatro1;
+                te2=R.string.teatro2;
+                r1t=R.string.rest1t;
+                r2t=R.string.rest2t;
+                r3t=R.string.rest3t;
+                r1=R.string.rest1;
+                r2=R.string.rest2;
+                r3=R.string.rest3;
+                ru1t=R.string.rumba1t;
+                ru2t=R.string.rumba2t;
+                ru3t=R.string.rumba3t;
+                ru1=R.string.rumba1;
+                ru2=R.string.rumba2;
+                ru3=R.string.rumba3;
+                tu1t=R.string.turis1t;
+                tu2t=R.string.turis2t;
+                tu3t=R.string.turis3t;
+                tu1=R.string.turis1;
+                tu2=R.string.turis2;
+                tu3=R.string.turis3;
+
+
+                tab1.setIndicator(lugares[l]);    //qué queremos que aparezca en las pestañas
+                tab1.setContent(R.id.ejemplo1); //definimos el id de cada Tab (pestaña)
+                tab2.setIndicator(lugares[l + 1]);
+                tab2.setContent(R.id.ejemplo2);
+                tab3.setIndicator(lugares[l + 2]);
+                tab3.setContent(R.id.ejemplo3);
+                TbH.addTab(tab1); //añadimos los tabs ya programados
+                TbH.addTab(tab2);
+                if (MainActivity.num !=1){
+                    TbH.addTab(tab3);
+                }
+
+                ImageView sit1 = (ImageView) findViewById(R.id.drw1);
+                ImageView sit2 = (ImageView) findViewById(R.id.drw2);
+                ImageView sit3 = (ImageView) findViewById(R.id.drw3);
+                TextView t1 = (TextView) findViewById(R.id.tit1);
+                TextView tx1 = (TextView) findViewById(R.id.tex1);
+                TextView t2 = (TextView) findViewById(R.id.tit2);
+                TextView tx2 = (TextView) findViewById(R.id.tex2);
+                TextView t3 = (TextView) findViewById(R.id.tit3);
+                TextView tx3 = (TextView) findViewById(R.id.tex3);
+                if(MainActivity.num ==0){
+                    setTitle(getText(R.string.Cine));
+                    sit1.setImageResource(R.drawable.cine1);
+                    t1.setText(c1t);
+                    tx1.setText(c1);
+                    sit2.setImageResource(R.drawable.cine2);
+                    t2.setText(c2t);
+                    tx2.setText(c2);
+                    sit3.setImageResource(R.drawable.cine3);
+                    t3.setText(c3t);
+                    tx3.setText(c3);
+                }else if(MainActivity.num ==1){
+                    setTitle(getText(R.string.Teatro));
+                    sit1.setImageResource(R.drawable.teatro1);
+                    t1.setText(t1t);
+                    tx1.setText(te1);
+                    sit2.setImageResource(R.drawable.teatro2);
+                    t2.setText(t2t);
+                    tx2.setText(te2);
+                }else if(MainActivity.num ==2){
+                    setTitle(getText(R.string.Restaurante));
+                    sit1.setImageResource(R.drawable.rest1);
+                    t1.setText(r1t);
+                    tx1.setText(r1);
+                    sit2.setImageResource(R.drawable.rest2);
+                    t2.setText(r2t);
+                    tx2.setText(r2);
+                    sit3.setImageResource(R.drawable.rest3);
+                    t3.setText(r3t);
+                    tx3.setText(r3);
+                }else if(MainActivity.num ==3){
+                    setTitle(getText(R.string.Rumba));
+                    sit1.setImageResource(R.drawable.rumba1);
+                    t1.setText(ru1t);
+                    tx1.setText(ru1);
+                    sit2.setImageResource(R.drawable.rumba2);
+                    t2.setText(ru2t);
+                    tx2.setText(ru2);
+                    sit3.setImageResource(R.drawable.rumba3);
+                    t3.setText(ru3t);
+                    tx3.setText(ru3);
+                }else if(MainActivity.num ==4){
+                    setTitle(getText(R.string.Turismo));
+                    sit1.setImageResource(R.drawable.turi1);
+                    t1.setText(tu1t);
+                    tx1.setText(tu1);
+                    sit2.setImageResource(R.drawable.turi2);
+                    t2.setText(tu2t);
+                    tx2.setText(tu2);
+                    sit3.setImageResource(R.drawable.turi3);
+                    t3.setText(tu3t);
+                    tx3.setText(tu3);
+
+                }}
         lstNombres.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String seleccion = tit[position+p];
                 num=position;
-                if(findViewById(R.id.lay2)!=null){
-
+                if (findViewById(R.id.lay2) != null){
                     Intent i = new Intent(MainActivity.this, MainActivity.class);
                     startActivity(i);
+                    finish();
+
+                 /*   TbH.getTabWidget().removeView(TbH.getTabWidget().getChildTabViewAt(2));
+
+                    TbH.getTabWidget().removeView(TbH.getTabWidget().getChildTabViewAt(1));
+
+                    TbH.getTabWidget().removeView(TbH.getTabWidget().getChildTabViewAt(0));*/
+
+
                 }else{
                 switch (position){
                     case 0:
